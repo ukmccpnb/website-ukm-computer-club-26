0fb469c57b6c248bf868dc97ff716d1e24b10506
urboysann <santikakumaraananda@gmail.com>
2026-08-13 12:20:58 +0800

    ganti latar

--- src/AppShell.jsx
+++ src/AppShell.jsx
@@ -5,6 +5,7 @@ import Navbar from './component/Navbar';
 import { Outlet } from 'react-router-dom';
 import Footer from './component/Footer';
 import Fab from './component/Fab';
+import bg_rectangle from './assets/img/ornamen/Rectangle.png';
 
 function AppShell() {
     useEffect(() => {
@@ -15,7 +16,15 @@ function AppShell() {
     }, []);
     
     return (
-        <div className="overflow-x-hidden overflow-y-hidden relative">
+        <div 
+            className="overflow-x-hidden overflow-y-hidden relative"
+            style={{ 
+                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.05)), url(${bg_rectangle})`, 
+                backgroundSize: 'cover', 
+                backgroundPosition: 'center', 
+                backgroundAttachment: 'fixed' 
+            }}
+        >
             <Navbar/>
             <Outlet/>
             <Fab/>
